67fb5c4cbc0ab4d2675f2c5a4ae702148159d178
Maciek Ploskonka <maciejploskonka@gmail.com>
2018-12-17 16:39:47 +0100

    add removing expenses from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -20,12 +20,13 @@ export const startAddExpense = (expenseData = {}) => {
       amount,
       createdAt
     }
-    return database.ref('expenses').push(expense).then((ref) => {
-      dispatch(addExpense({
-        id: ref.key,
-        ...expense
-      }))
-    })
+    return database.ref('expenses').push(expense)
+      .then((ref) => {
+        dispatch(addExpense({
+          id: ref.key,
+          ...expense
+        }))
+      })
   }
 }
 
@@ -33,6 +34,17 @@ export const removeExpense = ({ id }) => ({
   type: 'REMOVE_EXPENSE',
   id
 })
+
+export const startRemoveExpense = ({ id }) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove()
+      .then(() => {
+        dispatch(removeExpense({
+          id
+        }))
+      })
+  }
+}
 
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,12 @@
-import { addExpense, editExpense, removeExpense, startAddExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { 
+  addExpense,
+  editExpense, 
+  removeExpense, 
+  startAddExpense, 
+  setExpenses, 
+  startSetExpenses, 
+  startRemoveExpense 
+} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import configureMochStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
@@ -19,6 +27,22 @@ test('should setup remove expense action object', () => {
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: 'abc123'
+  });
+});
+
+test('should remove expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[2].id;
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    });
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val()).toBeFalsy();
+    done();
   });
 });
 
