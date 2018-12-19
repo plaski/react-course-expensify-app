76bedeea79775b490add740371fdb8e3b329f92b
Maciek Ploskonka <maciejploskonka@gmail.com>
2018-12-19 18:59:15 +0100

    Add private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Header from '../components/Header'
+import { Router, Route, Switch } from 'react-router-dom'
 import ExpenseDashboard from '../components/ExpenseDashboard'
 import AddExpense from '../components/AddExpense'
 import EditExpense from '../components/EditExpense'
 import Help from '../components/Help'
 import NotFound from '../components/NotFound'
+import LoginPage from '../components/LoginPage'
+import createHistory from 'history/createBrowserHistory'
+import PrivateRoute from './PrivateRoute'
+
+export const history = createHistory()
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
-      <Header />
       <Switch>
-        <Route exact path="/" component={ExpenseDashboard} />
-        <Route path="/create" component={AddExpense} />
-        <Route path="/edit/:id" component={EditExpense} />
+        <Route exact path="/" component={LoginPage} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboard} />
+        <PrivateRoute path="/create" component={AddExpense} />
+        <PrivateRoute path="/edit/:id" component={EditExpense} />
         <Route path="/help" component={Help} />
         <Route component={NotFound} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 )
 
 export default AppRouter
